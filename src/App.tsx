7618d91534f546c7f962c611e9c2fe27ed68cab2
Cuong Jackfruit <taduccuong2003@gmail.com>
2024-09-24 16:33:36 +0700

    - Tạ Đức Cường

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage.tsx";
+import HomePage from "../src/pages/HomePage.tsx";
 
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                {/* Define routes */}
-                <Route path="/" element={<HomePage />} />
+
+                <Route path="/Home" element={<HomePage />} />
             </Routes>
         </BrowserRouter>
     );
